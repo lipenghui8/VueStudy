8c07a3b6b5418934e7ca8b7a69e87ef9cdbf0140
lipenghuihenu <493284461@qq.com>
2022-01-13 16:48:14 +0800

    更新

---
+++ 3.使用Vue脚手架/myfirstvue/src/components/School.vue
@@ -0,0 +1,32 @@
+<!-- 组件采用大驼峰命名 -->
+<template>
+    <div>
+        <h2 class="school">学校名称:{{name}}</h2>
+        <h2>学校地址:{{address}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name:'河南大学',
+            address:'开封'
+        }
+    },
+    //生命周期 - 创建完成（访问当前this实例）
+    created() {
+        
+    },
+    //生命周期 - 挂载完成（访问DOM元素）
+    mounted() {
+        
+    }
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+    .school{
+        color:red;
+    }
+</style>
--- 3.使用Vue脚手架/myfirstvue/src/main.js
+++ 3.使用Vue脚手架/myfirstvue/src/main.js
@@ -1,8 +1,12 @@
+// 整个项目的入口文件
+// 引入Vue
 import Vue from 'vue'
+//引入App组件，即所有组件的父组件
 import App from './App.vue'
-
+//关闭生产提示
 Vue.config.productionTip = false
 
+//创建Vue实例对象
 new Vue({
   render: h => h(App),
 }).$mount('#app')
