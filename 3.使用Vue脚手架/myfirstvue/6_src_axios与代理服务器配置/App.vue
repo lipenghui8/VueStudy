47d4d8bd79875655a3a1b6060b04a2352ff7f550
lipenghuihenu <493284461@qq.com>
2022-01-22 23:49:51 +0800

    the new day

---
+++ 3.使用Vue脚手架/myfirstvue/6_src_axios与代理服务器配置/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Students/>
+        <Cars/>
+    </div>
+</template>
+
+<script>
+    
+    import Students from './components/Student.vue'
+    import Cars from './components/Car.vue'
+    //引入组件
+    export default {
+        name:'App',
+        components:{
+            Students,
+            Cars
+        }
+    }
+</script>
---
+++ 3.使用Vue脚手架/myfirstvue/6_src_axios与代理服务器配置/components/Student.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="getStudents">获取学生信息</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            getStudents(){
+                axios.get('http://localhost:8080/api/students').then(
+                    response=>{
+                        console.log('请求成功',response.data);
+                    },
+                    error=>{
+                        console.log('请求失败！',error.message)
+                    }
+                )
+            }
+        },
+    }
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+
+</style>
