6e00635594f636a101dafdd67ef41e03f33e5f7b
lipenghuihenu <493284461@qq.com>
2022-01-24 13:09:34 +0800

    默认插槽

--- 3.使用Vue脚手架/myfirstvue/src/App.vue
+++ 3.使用Vue脚手架/myfirstvue/src/App.vue
@@ -1,20 +1,41 @@
 <template>
-    <div>
-        <Students/>
-        <Cars/>
+    <div class="container">
+        <!-- 默认插槽，在组件标签中插入内容 -->
+        <Category title="美食">
+            <img style="width:200px" src="https://news.liph.top/resources/upload/image/20211226/1640507841027038512.jpg">
+        </Category>
+        <Category title="游戏">
+            <ul>
+                <li v-for="(item,index) in games" :key="index">{{item}}</li>
+            </ul>
+        </Category>
+        <Category title="电影">
+            <video controls autoplay="autoplay" loop width="200px" src="https://www.liph.top/skin/movie.mp4"></video>
+        </Category>
     </div>
 </template>
 
 <script>
     
-    import Students from './components/Student.vue'
-    import Cars from './components/Car.vue'
+    import Category from './components/Category.vue'
     //引入组件
     export default {
         name:'App',
         components:{
-            Students,
-            Cars
-        }
+            Category
+        },
+        data() {
+            return {
+                foods:['火锅','烧烤','小龙虾','牛排'],
+                games:['红色警戒','穿越火线','劲舞团','超级玛丽'],
+                films:['《教父》','《战狼》','《创业》','《三国演义》']
+            }
+        },
     }
 </script>
+<style>
+    .container{
+        display: flex;
+        justify-content: space-around;
+    }
+</style>
---
+++ 3.使用Vue脚手架/myfirstvue/src/components/Category.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="category">
+        <h3>{{title}}分类</h3>
+        <!-- solt定义一个插槽等待组件的使用者进行填充 -->
+        <slot>我是默认值，当使用者没有传递具体结构时，我会出现</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Category',
+    props:['title'],
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+.category{
+    width: 200px;
+    height: 300px;
+    overflow: hidden;
+}
+</style>
