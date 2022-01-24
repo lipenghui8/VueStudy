fed7ef66b269bdf31c29d8e4961e33630477f1a7
lipenghuihenu <493284461@qq.com>
2022-01-24 20:16:02 +0800

    vuex

--- 3.使用Vue脚手架/myfirstvue/src/App.vue
+++ 3.使用Vue脚手架/myfirstvue/src/App.vue
@@ -1,33 +1,17 @@
 <template>
     <div class="container">
-        <Category title="游戏">
-            <!-- 使用插槽中的数据，外面必须套上template -->
-            <template scope="{games}">
-                <ul>
-                    <li v-for="(item,index) in games" :key="index">{{item}}</li>
-                </ul>
-            </template>
-        </Category>
-        <hr>
-        <Category title="游戏">
-            <!-- 使用插槽中的数据，外面必须套上template -->
-            <template scope="lph">
-                <ul>
-                    <li v-for="(item,index) in lph.games" :key="index">{{item}}</li>
-                </ul>
-            </template>
-        </Category>
+        <Count/>
     </div>
 </template>
 
 <script>
     
-    import Category from './components/Category.vue'
+    import Count from './components/Count.vue'
     //引入组件
     export default {
         name:'App',
         components:{
-            Category
+            Count
         },
         
     }
--- 3.使用Vue脚手架/myfirstvue/src/main.js
+++ 3.使用Vue脚手架/myfirstvue/src/main.js
@@ -5,13 +5,22 @@ import Vue from 'vue'
 import App from './App.vue'
 // 引入插件
 import plugins from './plugins'
+//引入Vuex
+import Vuex from 'vuex'
+//引入store
+import store from './store'
 //关闭生产提示
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
 
 // 应用插件
 Vue.use(plugins)
 //创建Vue实例对象
 new Vue({
   render: h => h(App),
+  store,
+  beforeCreate(){
+    Vue.prototype.$bus=this
+  }
 }).$mount('#app')
